76fca81ad810915b2683cf39f0d5adbcf0597e29
y3414602-cpu <y3414602@gmail.com>
2025-09-02 06:06:19 -0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,14 @@
+// تأثير الكتابة للاسم
+const nameText = "youssef ammar";
+const nameElement = document.getElementById("animated-name");
+let index = 0;
+
+function typeName() {
+  if (index < nameText.length) {
+    nameElement.textContent += nameText.charAt(index);
+    index++;
+    setTimeout(typeName, 150);
+  }
+}
+
+typeName();
